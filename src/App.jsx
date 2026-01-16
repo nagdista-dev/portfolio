bf7815a170e6fc509a85a93021535bedf413bba9
Alnagdy <nagdista@gmail.com>
2026-01-16 19:47:26 +0200

    nagdista

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const App = () => {
-  return <div>Coming Soon</div>;
+  return <div className="w-full h-screen flex items-center justify-center">
+
+    <h2 className="text-2xl font-bold">Coming Soon</h2>
+  </div>;
 };
 
 export default App;
